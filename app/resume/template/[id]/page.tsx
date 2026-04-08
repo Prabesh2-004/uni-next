9e6356685fd51d22d5523bb4776f10259b881ab7
Prabesh Lamichhane <coder.bro2004@gmail.com>
2026-04-08 19:50:28 +0545

    Remove fetchCache constant from TemplatePage

--- app/resume/template/[id]/page.tsx
+++ app/resume/template/[id]/page.tsx
@@ -3,7 +3,7 @@ import Template1 from "@/components/cv-template/Template1";
 import Template2 from "@/components/cv-template/Template2";
 import Template3 from "@/components/cv-template/Template3";
 
-export const fetchCache = "force-no-store";
+
 
 export default async function TemplatePage({ params }: { params: { id: string } }) {
   const resume = await getResumeById(params.id);
