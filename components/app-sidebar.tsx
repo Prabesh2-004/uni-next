1aaadb80a07e76a510002b6692818082eab45cf9
Prabesh2-004 <coder.bro2004@gmail.com>
2026-05-22 20:49:55 +0545

    routing issue fixed

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -116,12 +116,12 @@ const data = {
   projects: [
     {
       name: "Live Events",
-      url: "admin/events",
+      url: "/admin/events",
       icon: Calendar,
     },
     {
       name: "Host Events",
-      url: "admin/host-events",
+      url: "/admin/host-events",
       icon: CalendarPlus,
     },
   ],
